b9ec5477129f1dc0b0aa67717ee6efc029077718
Jakob Cassiman <jakob.cassiman@gmail.com>
2019-11-23 22:44:53 -0500

    binary logistic omp

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
-class Data:
+import pandas as pd
+import numpy as np
 
-	def __init__(h5_path, n_rows='all'):
+class Data:
+	def __init__(self, h5_path, n_rows='all'):
 		self.path = h5_path
 		self.n_rows = n_rows
 		self.features = None
@@ -8,7 +10,7 @@ class Data:
 		self.unique_labels = None
 		self.relabels = None
 		
-	def load_data():
+	def load_data(self):
 		store = pd.HDFStore(self.path)
 		if self.n_rows == 'all':
 			feature_matrix_dataframe = store['rpkm']
@@ -18,12 +20,13 @@ class Data:
 			labels = store['labels'].iloc[:self.n_rows]
 		unique_labels = list(np.unique(labels))
 		store.close()
-		self.features = feature_matrix_dataframe
-		self.labels = labels
+		self.features = feature_matrix_dataframe.to_numpy()
+		self.labels = labels.to_numpy()
 		self.unique_labels = unique_labels
+		print("{} rows data loaded".format(self.n_rows))
 		return self.features, self.labels, self.unique_labels
 	
-	def relabel(label):
+	def relabel(self, label):
 		self.relabels = self.labels==label
 		
 	
--- log_omp.py
+++ log_omp.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 # implementation of orthogonal matching pursuit for logistic regression
 
 class LogisticOMP:
-	def __init__(n_nonzero_coefs, eps):
+	def __init__(self, n_nonzero_coefs, eps):
 		self.n_nonzero_coefs = n_nonzero_coefs
 		self.eps = eps
 		self.G = None
@@ -16,32 +16,32 @@ class LogisticOMP:
 		self.last_slctd_feature = None
 		self.last_slctd_correlation = None
 		self.feature_ranking = []
-		self.clf = LogisticRegression(random_state=0, solver='saga', n_jobs=-1)
+		self.clf = LogisticRegression(random_state=0, solver='saga', n_jobs=-1, max_iter=5000)
 		
-	def converged():
+	def converged(self):
 		# if enough non_zero_coefficients are found
-		if np.sum(G) >= self.n_nonzero_coefs:
-			return True
-		elif 
+		if np.sum(self.G) >= self.n_nonzero_coefs:
+			return True 
 		return False
 	
-	def g_inverse(x):
+	def g_inverse(self, x):
 		return expit(x)
 	
-	def calc_residual():
+	def calc_residual(self):
 		eta = np.matmul(self.X, self.beta)
 		y_hat = self.g_inverse(eta)
 		self.residual = y_hat - self.y
 	
-	def find_best_feature():
+	def find_best_feature(self):
 		# only calculate correlations for not-selected features
-		all_correlations = np.matmul(np.transpose(X), self.residual)
+		all_correlations = np.matmul(np.transpose(self.X), self.residual)
 		masked_correlations = np.multiply(all_correlations, np.invert(self.G))
 		self.last_slctd_feature = np.argmax(masked_correlations)
 		self.last_slctd_correlation = all_correlations[self.last_slctd_feature]
+		print("correlation of selected feature:", self.last_slctd_correlation)
 		self.feature_ranking.append(self.last_slctd_feature)
 	
-	def update_beta():
+	def update_beta(self):
 		slctd_X = self.X[:, self.G]
 		self.clf.fit(slctd_X, self.y)
 		learned_params = self.clf.coef_
@@ -49,23 +49,23 @@ class LogisticOMP:
 		for logistic_idx, weight_idx in enumerate(weights_idxs):
 			self.beta[weight_idx] = learned_params[logistic_idx]
 				
-	def fit(X, y):
+	def fit(self, X, y):
 		# expects y to be binary class labels
 		self.X = X
 		self.y = y
 		
-		self.G = np.zeros_like(X[0,:], dtype=bool)  # binary indx
-		self.beta = np.zeros_like(X[0, :])
+		self.G = np.zeros_like(self.X[0,:], dtype=bool)  # binary indx
+		self.beta = np.zeros_like(self.X[0, :])
 		while not self.converged():
 			self.calc_residual()
 			self.find_best_feature()
 			if self.last_slctd_correlation < self.eps:
 				return self
-			self.G[last_slctd_feature] = True
+			self.G[self.last_slctd_feature] = True
 			self.update_beta()
 			
 		return self
 		
-	def get_selected_feature_idxs():
+	def get_selected_feature_idxs(self):
 		return np.nonzero(self.G), self.feature_ranking
 	
