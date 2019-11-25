3d13910ca30b3e9afd870adfcdde98e9d89bf3d7
Jakob Cassiman <jakob.cassiman@gmail.com>
2019-11-24 22:12:01 -0500

    recommit

--- omp_feature_selection.py
+++ omp_feature_selection.py
@@ -5,47 +5,12 @@ import math
 import numpy as np
 # do feature selection for each class separately and take union of the selected features
 # usage: python omp_feature_selection.py <data_path> <n_features_to_select> <n_rows (optional)>
+# python omp_feature_selection.py ../train_data.h5 10
 
 def n_unique(array):
 	return np.unique(array).size
 	
 	
-def n_features_not_close_enough(n_classes, current_amount, target_n):
-	if math.abs(current_amount - target_n) > n_classes:
-		return True
-	return False
-	
-
-def remove_some(n_classes, current_amount, target_n, selections, new_set):
-	pass
-	
-	
-def add_some(n_classes, current_amount, target_n, selections, new_set):
-	pass
-	
-	
-def prune_to_correct_amount(selections, target_n):
-	n_classes = selections.shape[0]
-	current_amount = n_unique(selections)
-	current_cutoff_rank = selections.shape[0]
-	new_set = selections
-	while n_features_not_close_enough(n_classes, current_amount, target_n):
-		if current_amount > target_n:
-			new_set, current_amount = remove_some(n_classes, current_amount, target_n, selections, new_set)
-		elif current_amount < target_n:
-			new_set, current_amount = add_some(n_classes, current_amount, target_n, selections, new_set)
-	return new_set
-	
-		
-def simple_prune_to_correct_amount(selections, target_n):
-	print("pruning features...")
-	current_amount = n_unique(selections)
-	while current_amount > target_n:
-		selections = selections[:, :-1]  # remove at max <n_classes> features at a time
-		current_amount = n_unique(selections)
-	return selections	
-
-
 def select_features(data, n_features):
 	all_selections = np.empty((len(data.unique_labels), n_features), dtype=np.int16)
 	for i, label in enumerate(data.unique_labels):
