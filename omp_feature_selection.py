0599c62e4fc7f1a93fd871e2f5d46b6280c4b702
Jakob Cassiman <jakob.cassiman@gmail.com>
2019-11-24 20:36:50 -0500

    import numpy

--- omp_feature_selection.py
+++ omp_feature_selection.py
@@ -2,8 +2,9 @@ from log_omp import LogisticOMP
 from data import Data
 import sys
 import math
+import numpy as np
 # do feature selection for each class separately and take union of the selected features
-# usage: omp_feature_selection <data_path> <n_features_to_select> <n_rows (optional)>
+# usage: python omp_feature_selection.py <data_path> <n_features_to_select> <n_rows (optional)>
 
 def n_unique(array):
 	return np.unique(array).size
@@ -13,10 +14,16 @@ def n_features_not_close_enough(n_classes, current_amount, target_n):
 	if math.abs(current_amount - target_n) > n_classes:
 		return True
 	return False
+	
 
-def add_or_remove_some(n_classes, current_amount, target_n, new_set):
-	n_features_to_add_or_remove
-
+def remove_some(n_classes, current_amount, target_n, selections, new_set):
+	pass
+	
+	
+def add_some(n_classes, current_amount, target_n, selections, new_set):
+	pass
+	
+	
 def prune_to_correct_amount(selections, target_n):
 	n_classes = selections.shape[0]
 	current_amount = np.unique(selections)
@@ -24,11 +31,11 @@ def prune_to_correct_amount(selections, target_n):
 	new_set = selections
 	while n_features_not_close_enough(n_classes, current_amount, target_n):
 		if current_amount > target_n:
-			new_set, current_amount = remove_some(n_classes, current_amount, target_n, selections)
+			new_set, current_amount = remove_some(n_classes, current_amount, target_n, selections, new_set)
 		elif current_amount < target_n:
-			new_set
-		new_set, current_amount = add_or_remove_some(n_classes, current_amount, target_n, new_set)
-
+			new_set, current_amount = add_some(n_classes, current_amount, target_n, selections, new_set)
+	return new_set
+	
 		
 def simple_prune_to_correct_amount(selections, target_n):
 	current_amount = np.unique(selections)
