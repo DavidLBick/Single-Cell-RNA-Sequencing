8f5bdf660f8dc9f454cbc8bd6789e771cb4b8f9a
Jakob Cassiman <jakob.cassiman@gmail.com>
2019-11-27 10:56:52 -0500

    init embeddings evaluation with  clustering scores

---
+++ eval_embeddings.py
@@ -0,0 +1,4 @@
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+# k-means ++ clustering: https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+# adjusted random index: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_rand_score.html
