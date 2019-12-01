1e213c99ce2b7976d3e32fde45459dabe3848826
DavidLBick <davidlbick@gmail.com>
2019-12-01 16:16:37 -0500

    Correct config changes

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ MODELS_PATH = './saved_model/'
 SAVE_MODEL_NAME = "baseline_model"
 N_EPOCHS = 100
 TRAIN_DATA_NP_ARRAY = None # need to set this to load the np.array features
+DATA_PATH = './ml_10701_ps5_data/'
+TRAIN_DATA_PATH = DATA_PATH + 'train_data.h5'
+TEST_DATA_PATH = DATA_PATH + 'test_data.h5'
