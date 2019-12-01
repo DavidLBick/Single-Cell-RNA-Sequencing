97b96c7807c75f1dcf6bfcfc8c401ae42e2eec53
DavidLBick <davidlbick@gmail.com>
2019-12-01 15:05:58 -0500

    Changed dataloading to load np array Magic data rather than h5 files

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-# Set constants, magic numbers, variables, settings, etc. here 
+# Set constants, magic numbers, variables, settings, etc. here
 BATCH_SIZE = 64
 MODELS_PATH = './saved_model/'
 SAVE_MODEL_NAME = "baseline_model"
 N_EPOCHS = 100
+TRAIN_DATA_NP_ARRAY = None # need to set this to load the np.array features
