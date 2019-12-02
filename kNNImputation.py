38e84a0f2e4eb448c495cd3cc2b5e17687068382
KayEs141193 <kushal.saharan@gmail.com>
2019-12-02 18:49:01 -0500

    Added method to directly run imputation and save data

--- kNNImputation.py
+++ kNNImputation.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from missingpy import KNNImputer
+import numpy as np
 
 class KNN_impute:
         
-    def __init__(self,missing_val_rep=0.0, k = 10, copy = False):
+    def __init__(self,missing_val_rep=0.0, k = 1, copy = False):
         
         self.missing_val_rep = missing_val_rep
         self.imputer = KNNImputer(missing_val_rep,k,copy=copy)
@@ -16,7 +17,7 @@ class KNN_impute:
         
         X['labels'] = y
         X_median = X.groupby(by='labels').apply(add_median)
-        print(X_median)
+        #print(X_median)
         X.drop(columns=['labels'],inplace=True)
         X_median.drop(columns=['labels'],inplace=True)
 
@@ -26,8 +27,43 @@ class KNN_impute:
     def transform(self,X):
         return self.imputer.transform(X)
 
+def load_df(path):
+    store = pd.HDFStore(path)
+    X, y = store['rpkm'], store['labels']
+    
+    return X,y
+
+def join_sets(X1,y1,X2,y2):
+    X, y = pd.concat([X1, X2]), pd.concat([y1, y2]) 
+    #return X.reset_index().drop(columns = ['index']), y.reset_index().iloc[:,1]
+    return X, y
+    
+def split_sets(X,y,l1):
+    return X.iloc[:l1,:], y.iloc[:l1], X.iloc[l1:,:], y.iloc[l1:]
+    
+def run_imputation(train_path, test_path=None):
+    
+    name = 'kNNImputedTrain.npy'
+    X, y = load_df(train_path)
+    
+    if test_path is not None:
+        l1 = len(X)
+        X, y = join_sets(X,y,*load_df(test_path))
+        name = 'kNNImputedTest.npy'
+    
+    imputer = KNN_impute()
+    imputer.fit(X,y)
+    X_transformed = imputer.transform(X)
+    
+    if test_path is not None:
+        _, _, X_transformed, _  = split_sets(X,y,l1)
+    
+    np.save(name, X_transformed)
+    
+    
 
 if __name__ == '__main__':
+    
     
     X = pd.DataFrame([ [0,100,1],
                        [5,120,1],
@@ -45,4 +81,7 @@ if __name__ == '__main__':
     #print(X)
     #print(obj)
     print(obj.transform(X))
+    
+    
+    #run_imputation('blah','blah')
     
